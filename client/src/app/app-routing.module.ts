af81eff5ea8a4929a3ed7e219f916a5bf8579286
Persevere You <422913988@qq.com>
2019-10-21 19:43:25 +0800

    second

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroListComponent } from './hero/hero-list/hero-list.component';
+import { HeroTopComponent } from './hero/hero-top/hero-top.component';
+import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
+import { HeroAddComponent } from './hero/hero-add/hero-add.component';
+import { HeroEditComponent } from './hero/hero-edit/hero-edit.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hero-list',
+    component: HeroListComponent,
+  },
+  {
+    path: 'hero-top',
+    component: HeroTopComponent,
+  },
+  {
+    path: 'hero-detail/:id',
+    component: HeroDetailComponent,
+  },
+  {
+    path: 'hero-add',
+    component: HeroAddComponent,
+  },
+  {
+    path: 'hero-edit/:id',
+    component: HeroEditComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'hero-list',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
